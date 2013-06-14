3a3531766ef9de3a37fd99563e74ebff1b7fb615
Konstantin Zverev <konstantin.zverev@gmail.com>
2013-06-14 15:22:13 +0200

    Made restore_links.sh more careful with removing files

--- restore_links.sh
+++ restore_links.sh
@@ -3,7 +3,8 @@
 for file in `ls -1` ; do
     if [[ ! -d $file ]] ; then
         # it's a regular file, let's link it to the home directory
-        ln $HOME/.${file} $file
+        if [[ -r $HOME/.${file} ]] ; then
+            rm $file && ln $HOME/.${file} $file
+        fi
     fi
 done
-
