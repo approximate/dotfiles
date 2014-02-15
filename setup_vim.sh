1f5bd3cb725e3d53c5fbb1742cb30732bc552106
Konstantin Zverev <konstantin.zverev@gmail.com>
2014-02-15 16:55:57 -0500

    fix: corrected directory in ViM setuo script, updated one ViM repo

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.vim/bundle ~/.vim/.cache || exit 1
-git clone http://github.com/gmarik/vundle || exit 1
+cd ~/.vim/bundle || exit 1
+git clone http://github.com/gmarik/vundle
 cd ~/.vim/.cache || exit 1
 mkdir -p backup undo swap fuzzyfind ctrlp yankring || exit 1
 vim +BundleInstall +qall
