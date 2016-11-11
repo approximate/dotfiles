df8b62d6d64bd5b2025cd5134d8455992f7884ed
Konstantin Zverev <konstantin.zverev@gmail.com>
2016-11-11 12:18:31 +0100

    fix: updated Vundle references in Vim setup script

--- setup_vim.sh
+++ setup_vim.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.vim/bundle ~/.vim/.cache || exit 1
 cd ~/.vim/bundle || exit 1
-git clone http://github.com/gmarik/vundle
+git clone http://github.com/VundleVim/Vundle.vim
 cd ~/.vim/.cache || exit 1
 mkdir -p backup undo swap fuzzyfind ctrlp yankring || exit 1
-vim +BundleInstall +qall
+vim +PluginInstall +qall
